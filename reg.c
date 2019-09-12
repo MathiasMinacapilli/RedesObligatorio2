be56774ab2e496a3ce053ce47135d9bd1c80bb40
Carolina Vega <carovegacamejo@gmail.com>
2019-09-11 21:20:09 -0300

    Agregado reg.c que encuentra la tag inicial, el nombre de usuario y la contraseña

--- reg.c
+++ reg.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <sys/types.h>   
 #include <regex.h>
+#include <string.h>
 
 int main()
 {
@@ -12,25 +13,50 @@ int main()
  * return 1 for match, 0 for no match
  */
 
-char string[] = "a001 loGin matias saitam\n";
+char * source = "a001 loGin matias saitam\n";
+char * regexString = "(([a-z]|[0-9])+) login ([a-z]+) ([a-z]+)";
 int    status;
-regex_t  re; 
+regex_t  regexCompiled; 
+size_t maxGroups = 10;
+regmatch_t groupArray[maxGroups];
+char * cursor;
+char * usuario = "";
 
-    if (regcomp(&re, "([a-z]|[A-Z]|[0-9])+ (login) ([a-z]|[A-Z])+ ([a-z]|[A-Z])+", REG_EXTENDED|REG_NOSUB|REG_ICASE) != 0) {//devuelve 0 si compila la re
-        printf("No compilo");
-        return(0);      /* report error */
+  //Compila la expresion regular  
+  if (regcomp(&regexCompiled, regexString, REG_EXTENDED|REG_ICASE))
+    {
+      printf("No se pudo compilar la expresión regular\n");
+      return 1;
+    };
+
+  cursor = source;
+     {
+      if (regexec(&regexCompiled, cursor, maxGroups, groupArray, 0))
+        return(0);  // No more matches
+
+      unsigned int g = 0;
+      unsigned int offset = 0;
+      for (g = 0; g < maxGroups; g++)
+        {
+          if (groupArray[g].rm_so == (size_t)-1){
+            printf(usuario);
+            regfree(&regexCompiled);
+            return(0);  // No more groups
+          }
+          
+          if (g == 0)
+            offset = groupArray[g].rm_eo;
+          
+          char cursorCopy[strlen(cursor) + 1];
+          strcpy(cursorCopy, cursor);
+          cursorCopy[groupArray[g].rm_eo] = 0;
+          printf("Group %u: [%2u-%2u]: %s\n",
+                 g, groupArray[g].rm_so, groupArray[g].rm_eo,
+                 cursorCopy + groupArray[g].rm_so);
+        }
+      cursor += offset;
     }
-    status = regexec(&re, string, (size_t) 0, NULL, 0); //devuelve 0 si matchea
-    regfree(&re);
-    if (status != 0) {
-        printf("No matcheo");
-        return(0);      /* report error */
-    }
-    
-    
-    
-    printf("matcheo");
-     return(1);
+  return(1);
 
 
 return 0;
