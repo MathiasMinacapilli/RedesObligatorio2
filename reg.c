db66dd60a52c610c560854f19a3da65cb4fd5883
carolinavegacam <53955261+carolinavegacam@users.noreply.github.com>
2019-09-13 01:32:13 -0300

    Update reg.c

--- reg.c
+++ reg.c
@@ -1,63 +1,40 @@
 #include <stdio.h>
-#include <sys/types.h>   
-#include <regex.h>
 #include <string.h>
-
-int main()
-{
-
-/*
- * Match string against the extended regular expression in
- * pattern, treating errors as no match.
- *
- * return 1 for match, 0 for no match
- */
-
-char * source = "a001 loGin matias saitam\n";
-char * regexString = "(([a-z]|[0-9])+) login ([a-z]+) ([a-z]+)";
-int    status;
-regex_t  regexCompiled; 
-size_t maxGroups = 10;
-regmatch_t groupArray[maxGroups];
-char * cursor;
-char * usuario = "";
-
-  //Compila la expresion regular  
-  if (regcomp(&regexCompiled, regexString, REG_EXTENDED|REG_ICASE))
+#include <ctype.h>
+#include <stdlib.h> 
+ 
+int main(int argc, char const *argv[])
+{ 
+    char *source = "a001 login matias saitam";
+    int length = (int)strlen(source); //sizeof(source)=sizeof(char *) = 4 on a 32 bit implementation
+    int i = 0;
+    int encontre_usuario = 0;
+    int cant_espacios = 0;
+    while (i < length && encontre_usuario == 0) 
     {
-      printf("No se pudo compilar la expresión regular\n");
-      return 1;
-    };
-
-  cursor = source;
-     {
-      if (regexec(&regexCompiled, cursor, maxGroups, groupArray, 0))
-        return(0);  // No more matches
-
-      unsigned int g = 0;
-      unsigned int offset = 0;
-      for (g = 0; g < maxGroups; g++)
-        {
-          if (groupArray[g].rm_so == (size_t)-1){
-            printf(usuario);
-            regfree(&regexCompiled);
-            return(0);  // No more groups
-          }
-          
-          if (g == 0)
-            offset = groupArray[g].rm_eo;
-          
-          char cursorCopy[strlen(cursor) + 1];
-          strcpy(cursorCopy, cursor);
-          cursorCopy[groupArray[g].rm_eo] = 0;
-          printf("Group %u: [%2u-%2u]: %s\n",
-                 g, groupArray[g].rm_so, groupArray[g].rm_eo,
-                 cursorCopy + groupArray[g].rm_so);
+        if(source[i] == ' '){
+            cant_espacios++;
         }
-      cursor += offset;
+        
+        if(source[i] == ' ' && cant_espacios == 2){
+            encontre_usuario = 1;
+        }
+//         printf("%c", source[i]);
+        i++;
     }
-  return(1);
-
-
+    
+    int j = i;
+    while(source[j] != ' '){
+        /*char * caracter = &source[i];
+        strcpy(usuario, caracter);
+        printf("%c", source[i]);*/
+        j++;
+    }
+    int largo = j-i;
+    char usuario[largo];
+    for(int k=i; k<j; k++){
+        usuario[k-i]= source[k];
+    } 
+    printf("%s", usuario);
 return 0;
 }
